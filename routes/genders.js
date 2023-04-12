6fb3781b6843d30d38359a1df4c2c432d459a629
Alexandre <Alexandresossou@gmail.com>
2023-04-12 11:44:58 +0200

    AS 11h44 mercredi

---
+++ routes/genders.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+require('../models/connection');
+const Gender = require('../models/genders');
+const app = express();
+
+// Route to get all genders
+app.get('/genders', async (req, res) => {
+  try {
+    const genders = await Gender.find();
+    res.json({result : true, genders: genders});
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ result: false, error: 'Failed to retrieve genders' });
+  }
+});
+
+// Route Post for Genders
+
+router.post('/genders', async (req, res) => {
+  try {
+    const { id } = req.body;
+    const gender = await Gender.findById(id);
+    if(!gender){
+      return res.json({ result: false, error: 'Gender not found' });
+    }
+    res.json({ result: true, gender: gender });
+  } catch (err) {
+    res.json({ result: false, error: 'Failed to retrieve gender' });
+  }
+});
+
+// Route put for Genders
+
+
+
+module.exports = router;
--- routes/orientations.js
+++ routes/orientations.js
@@ -5,15 +5,50 @@ const Orientation = require('../models/orientations');
 const app = express();
 
 // Route to get all orientations
-app.get('/orientations', async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const orientations = await Orientation.find();
-    res.json(orientations);
+    res.json({ result: true, orientations: orientations });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Failed to retrieve orientations' });
+    res.status(500).json({ result: false, error: 'Failed to retrieve orientations' });
   }
 });
 
+
+
+// Route Post for orientations
+
+router.post('/orientations', async (req, res) => {
+  try {
+    const { id } = req.body;
+    const orientation = await Orientation.findById(id);
+    if (!orientation) {
+      return res.json({ result: false, error: 'Orientation not found' });
+    }
+    res.json({ result: true, orientation });
+  } catch (error) {
+    console.error(error);
+    res.json({ result: false, error: 'Failed to retrieve orientation' });
+  }
+});
+
+// Route to update orientation
+
+router.put('/orientations/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { value } = req.body;
+    const updatedOrientation = await Orientation.findByIdAndUpdate(
+      id,
+      { value },
+      { new: true }
+    );
+    res.json({ result: true, orientation: updatedOrientation });
+  } catch (error) {
+    console.error(error);
+    res.json({ result: false, error: 'Failed to update orientation' });
+  }
+});
 
 module.exports = router;
