af5b9c7dadac29cdb01be4c1efd7efb2a8e2a88b
Alexandre <Alexandresossou@gmail.com>
2023-04-13 09:51:58 +0200

    4

--- routes/orientations.js
+++ routes/orientations.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 require('../models/connection');
 const Orientation = require('../models/orientations');
-const app = express();
 
 // GET /orientations
 
